59cf9f4bbb953bab659e0b2390729c11aafbcf60
Sahilkhatri68 <86000524+Sahilkhatri68@users.noreply.github.com>
2025-07-20 21:25:47 -0400

    Initial Next.js + Tailwind + TypeScript setup

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', minHeight: '100vh', alignItems: 'center', justifyContent: 'center', fontSize: '2rem', fontWeight: 'bold' }}>
+      Hello World from Next.js!
+    </div>
+  );
+}
